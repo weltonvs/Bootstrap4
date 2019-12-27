6d0a7e9dfa95a46bfa219c0cb0b73e5d68e5263f
Welton Vieira dos Santos <welton@gmail.com>
2019-12-27 10:27:07 +0100

    Validando los campos obrigatorios del formulario.

--- Capitulo 7 - Formularios/formulario.php
+++ Capitulo 7 - Formularios/formulario.php
@@ -1,4 +1,33 @@
 <?php
-    echo("exito");
+    //$nombre = $_POST["nombre"];
+    $Error = '';//Variable que devolverá el error cometido.
+    // Validando el Nombre.
+    if(empty($_POST["nombre"])){//Probar si al campo nombre esta vacio.
+        $Error = 'Ingrese un nombre</br>';
+    } else {
+        //Caso de que la variable no esté vacia
+        $nombre = $_POST["nombre"];
+        $nombre = filter_var($nombre,FILTER_SANITIZE_STRING);//limpiar la variable de caracteres basura.
+    }
 
+    // Validando el E-mail.
+    if(empty($_POST["email"])){
+        $Error .= 'Ingrese el correo electronico</br>';
+    } else {
+        $email = $_POST["email"];
+        if(!filter_var($email,FILTER_VALIDATE_EMAIL)){//Verificar si hay correo valido.
+            $Error .= 'Ingrese un correo electronico válido</br>';
+        } else {
+            $email = filter_var($email, FILTER_SANITIZE_STRING);//Eliminar caracteres estraños.
+        }
+    }
+
+    //Validando el mensaje.
+    if(empty($_POST["mensaje"])){//Probar si al campo nombre esta vacio.
+        $Error .= 'Ingrese un mensaje</br>';
+    } else {
+        //Caso de que la variable no esté vacia
+        $mensaje = $_POST["mensaje"];
+        $mensaje = filter_var($mensaje,FILTER_SANITIZE_STRING);//limpiar la variable de caracteres basura.
+    }
 ?>
